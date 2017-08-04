93782987a7ca525e3ecdfd8f20be79896baeb7dc
Arkadiusz Klein <arek.klein@limebrains.com>
2017-08-04 14:19:50 +0200

    Bumpversion

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='pytrojmiastopl',
-    version='0.0.2',
+    version='0.0.1',
     description="pytrojmiastopl - python wrapper for trojmiasto.pl",
     author='LimeBrains',
     author_email='mail@limebrains.com',
